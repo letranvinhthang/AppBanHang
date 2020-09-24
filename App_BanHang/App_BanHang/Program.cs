2da2f4bc96f34e087058d94595f2651063e5808a
letranvinhthang <vinhthang0600@gmail.com>
2020-09-24 10:27:58 +0700

    app

---
+++ App_BanHang/App_BanHang/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using App_BanHang.Views;
+
+namespace App_BanHang
+{
+    
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+<<<<<<< HEAD
+            Application.Run(new frm_dangnhap());
+=======
+            Application.Run(new frm_spyeuthich());
+>>>>>>> 422a76d0243ad760975c3bd29455323ec79b65c4
+        }
+    }
+}
---
+++ App_BanHang/App_BanHang/Views/frm_giohang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_BanHang.Views
+{
+    public partial class frm_giohang : Form
+    {
+        public frm_giohang()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_dangnhap_Click(object sender, EventArgs e)
+        {
+            frm_taikhoan i = new frm_taikhoan();
+            i.MdiParent = this;
+            i.Show();
+        }
+
+        private void frm_giohang_Load(object sender, EventArgs e)
+        {
+            //this.IsMdiContainer = true;
+        }
+    }
+}
